ed14eed0c8b06259e71d0b3289338a7097314460
Alex Bundy <abundy@opuslogica.com>
2018-12-26 16:05:34 -0700

    Narrow styling of header/footer.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { WhatIsOrchidComponent } from './what-is-orchid/what-is-orchid.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MainNavComponent } from './main-nav/main-nav.component';
 import { WhyOrchidComponent } from './why-orchid/why-orchid.component';
 import { DevResourcesComponent } from './dev-resources/dev-resources.component';
@@ -20,6 +21,7 @@ import { HomeComponent } from './home/home.component';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule
   ],
   providers: [],
