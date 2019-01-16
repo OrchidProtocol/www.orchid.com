f542a4c6c2ed83e6addd099cae2615d1c026be0f
rookie1024 <rookie1286@gmail.com>
2019-01-15 16:59:04 -0800

    Tweaked the homepage and started on /what-is-orchid - Fixed a couple minor style issues on the homepage - Added styles for .header-splash elements - Added page titles based on route URLs - Started filling out /what-is-orchid

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,54 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from "@angular/core";
+import {Title} from "@angular/platform-browser";
+import {
+  ActivatedRoute,
+  NavigationEnd,
+  Router,
+  RouterState
+} from "@angular/router";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"]
 })
-export class AppComponent {
-  title = 'orchid-www';
+export class AppComponent implements OnInit {
+  title = "orchid-www";
+
+  titleMap: Map<string, string> = new Map<string, string>([
+    ["/", "Orchid"],
+    ["/what-is-orchid", "What is Orchid?"],
+    ["/why-orchid", "Why Orchid"],
+    ["/dev-resources", "Dev Resources"],
+  ]);
+
+  public constructor(private router: Router, private titleService: Title) {}
+
+  ngOnInit(): void {
+    this.router.events.subscribe((evt) => {
+      if (!(evt instanceof NavigationEnd)) return;
+
+      console.log(this.titleMap, this.router.routerState.snapshot.url);
+
+      // TODO: this feels very bad
+      this.titleService.setTitle(
+          this.titleMap.get(this.router.routerState.snapshot.url));
+    }, (err) => {}, () => {});
+  }
+
+  getBreadcrumbs(state, parent: ActivatedRoute): string[] {
+    const data = [];
+
+    if (parent && parent.snapshot.data && parent.snapshot.data.title) {
+      data.push(parent.snapshot.data.title);
+    }
+
+    if (state && parent) {
+      data.push(...this.getBreadcrumbs(state, state.firstChild(parent)));
+    }
+
+    return data;
+  }
+
+  public setTitle(newTitle: string) { this.titleService.setTitle(newTitle); }
 }
