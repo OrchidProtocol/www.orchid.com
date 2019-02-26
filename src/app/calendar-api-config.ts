d1db9b780a95444e8670c39fc41d4b814cd93d7e
ryans <ryans@opuslogica.com>
2019-02-26 11:52:55 -0800

    Configured the calendar API.

---
+++ src/app/calendar-api-config.ts
@@ -0,0 +1,4 @@
+export module CalendarApiConfig {
+  export const CAL_ID  = "orchid.com_54cnq31nva33j10amtejovie74@group.calendar.google.com";
+  export const API_KEY = "AIzaSyDo_hYxVWBPn00H_6xal7fiSCKOYQ73vgg";
+}
