ce5ee84c48d154d45f99e0c5f8e15d2611f3e94d
ryans <ryans@opuslogica.com>
2019-03-01 15:07:22 -0800

    Fixed the whitepaper button on /network.

---
+++ src/app/whitepaper.ts
@@ -0,0 +1,15 @@
+export function downloadWhitepaper() {
+  let whitepaper = "";
+
+  if (window && "location" in window && "protocol" in window.location &&
+      "pathname" in window.location && "host" in window.location) {
+    whitepaper = window.location.protocol + "//" + window.location.host +
+                 "/assets/whitepaper/whitepaper.pdf";
+  }
+  else {
+    whitepaper = "https://github.com/orchidsource/whitepaper";
+  }
+
+  console.log("Here's the whitepaper: ", whitepaper);
+  window.location.href = whitepaper;
+}
