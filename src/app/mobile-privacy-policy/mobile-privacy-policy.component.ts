68c2a28765be32391962492672f33e12c793b370
Nathan Handler <nhandler@orchid.com>
2019-08-16 14:05:04 -0700

    Add Mobile App Privacy Policy

---
+++ src/app/mobile-privacy-policy/mobile-privacy-policy.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-privacy-policy',
+  templateUrl: './mobile-privacy-policy.component.html',
+  styleUrls: ['./mobile-privacy-policy.component.scss']
+})
+export class MobilePrivacyPolicyComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/routes.ts
+++ src/app/routes.ts
@@ -10,6 +10,9 @@ import {PageLayoutComponent} from "./page-layout/page-layout.component";
 import {
   PrivacyPolicyComponent
 } from "./privacy-policy/privacy-policy.component";
+import {
+  MobilePrivacyPolicyComponent
+} from "./mobile-privacy-policy/mobile-privacy-policy.component";
 import {ServiceTermsComponent} from "./service-terms/service-terms.component";
 import {WhyOrchidComponent} from "./why-orchid/why-orchid.component";
 
@@ -30,7 +33,9 @@ export const routes: Routes = [
       {path: "events", component: EventsComponent},
       {path: "events.html", redirectTo: "events"},
       {path: "privacy-policy", component: PrivacyPolicyComponent},
+      {path: "mobile-privacy-policy", component: MobilePrivacyPolicyComponent},
       {path: "privacy-policy.html", redirectTo: "privacy-policy"},
+      {path: "mobile-privacy-policy.html", redirectTo: "mobile-privacy-policy"},
       {path: "service-terms", component: ServiceTermsComponent},
       {path: "service-terms.html", redirectTo: "service-terms"},
     ]
