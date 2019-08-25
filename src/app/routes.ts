cb1f5273ac479425a7f911e08366c39648a20553
Brian J. Fox <bfox@opuslogica.com>
2019-08-25 13:05:12 -0700

    Complete

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -8,50 +8,52 @@ import {FaqsComponent} from "./faqs/faqs.component";
 import {HomeComponent} from "./home/home.component";
 import {PageLayoutComponent} from "./page-layout/page-layout.component";
 import {
-  PrivacyPolicyComponent
+    PrivacyPolicyComponent
 } from "./privacy-policy/privacy-policy.component";
 import {
-  MobilePrivacyPolicyComponent
+    MobilePrivacyPolicyComponent
 } from "./mobile-privacy-policy/mobile-privacy-policy.component";
 import {ServiceTermsComponent} from "./service-terms/service-terms.component";
 import {VisionComponent} from "./vision/vision.component";
 
 export const routes: Routes = [
-  {
-    path: "",
-    component: PageLayoutComponent,
-    children: [
-      {path: "", component: HomeComponent},
-      {path: "app", component: AboutAppComponent},
-      {path: "app.html", redirectTo: "app"},
-      {path: "network", component: AboutNetworkComponent},
-      {path: "network.html", redirectTo: "network"},
-      {path: "vision", component: VisionComponent},
-      {path: "vision.html", redirectTo: "vision"},
-      {path: "faqs", component: FaqsComponent},
-      {path: "faqs.html", redirectTo: "faqs"},
-      {path: "events", component: EventsComponent},
-      {path: "events.html", redirectTo: "events"},
-      {path: "privacy-policy", component: PrivacyPolicyComponent},
-      {path: "mobile-privacy-policy", component: MobilePrivacyPolicyComponent},
-      {path: "privacy-policy.html", redirectTo: "privacy-policy"},
-      {path: "mobile-privacy-policy.html", redirectTo: "mobile-privacy-policy"},
-      {path: "service-terms", component: ServiceTermsComponent},
-      {path: "service-terms.html", redirectTo: "service-terms"},
-    ]
-  },
-  {
-    path: "",
-    component: PageLayoutComponent,
-    data: {purpleLayout: true},
-    children: [
-      {
-        path: "company",
-        component: AboutCompanyComponent,
-        data: {purpleLayout: true}
-      },
-      {path: "company.html", redirectTo: "company"},
-    ]
-  },
-  {path: "index.html", redirectTo: ""},
+    {
+	path: "",
+	component: PageLayoutComponent,
+	children: [
+	    {path: "", component: HomeComponent},
+	    {path: "app", component: AboutAppComponent},
+	    {path: "app.html", redirectTo: "app"},
+	    {path: "network", component: AboutNetworkComponent},
+	    {path: "network.html", redirectTo: "network"},
+	    {path: "vision", component: VisionComponent},
+	    {path: "vision.html", redirectTo: "vision"},
+	    {path: "about-us", component: AboutCompanyComponent},
+	    {path: "about-us.html", redirectTo: "about-us"},
+	    {path: "faqs", component: FaqsComponent},
+	    {path: "faqs.html", redirectTo: "faqs"},
+	    {path: "events", component: EventsComponent},
+	    {path: "events.html", redirectTo: "events"},
+	    {path: "privacy-policy", component: PrivacyPolicyComponent},
+	    {path: "mobile-privacy-policy", component: MobilePrivacyPolicyComponent},
+	    {path: "privacy-policy.html", redirectTo: "privacy-policy"},
+	    {path: "mobile-privacy-policy.html", redirectTo: "mobile-privacy-policy"},
+	    {path: "service-terms", component: ServiceTermsComponent},
+	    {path: "service-terms.html", redirectTo: "service-terms"},
+	]
+    },
+    {
+	path: "ignore",
+	component: PageLayoutComponent,
+	data: {purpleLayout: true},
+	children: [
+	    {
+		path: "company",
+		component: AboutCompanyComponent,
+		data: {purpleLayout: true}
+	    },
+	    {path: "company.html", redirectTo: "company"},
+	]
+    },
+    {path: "index.html", redirectTo: ""},
 ];
