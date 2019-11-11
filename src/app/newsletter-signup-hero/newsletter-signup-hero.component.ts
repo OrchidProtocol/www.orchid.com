4e42f1362ceb2ffc9345103f523318aead782aba
Tyler Durrett <hi@tylerdurrett.com>
2019-11-11 16:25:00 -0600

    Add new version of the newsletter signup component
    This creates a new copy of the existing newsletter component, with styling differences specific to the homepage hero section. I considered refactoring the existing newsletter component so that it could work at the footer and/ or the hero section, but opted instead for the simplicity of duplicating the component because I don't expect this to be a long-lived component.

---
+++ src/app/newsletter-signup-hero/newsletter-signup-hero.component.ts
@@ -0,0 +1,58 @@
+import { HttpClient } from "@angular/common/http";
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "app-newsletter-signup-hero",
+  templateUrl: "./newsletter-signup-hero.component.html",
+  styleUrls: ["./newsletter-signup-hero.component.scss"],
+})
+
+export class NewsletterSignupHeroComponent implements OnInit {
+  _email: string     = "";
+  consented: boolean = false;
+  error: string      = "";
+  success: string    = "";
+  submitted: boolean = false;
+  blink_box: boolean = false;
+  showFull: boolean  = false;
+
+  get email() { return this._email; }
+
+  set email(val: string) {
+    this._email   = val;
+    this.showFull = !!this._email;
+    if (!this.showFull) this.consented = false;
+  }
+
+  constructor(private http: HttpClient) {}
+
+  submit() {
+    const mailchimp_add = "https://ik396c7x0k.execute-api.us-west-2.amazonaws.com/default/mailchimp?email=";
+    const mailchimp_url = mailchimp_add + encodeURIComponent(this.email || "");
+
+    if (!this.consented) {
+      this.blink_box = true;
+      return false;
+    } else {
+      this.blink_box = false;
+    }
+
+    this.error = "";
+
+    this.http.get(mailchimp_url)
+      .subscribe(
+        response => {
+	  if (response["status"] == "pending") {
+	    this.submitted = true;
+	    this.success = "Great! Now please check your email and confirm."
+	  } else if (response["detail"]) {
+	    this.error = response["detail"];
+	  }
+        },
+        error => {
+          this.error = "Sorry, an error occurred.";
+        });
+  }
+
+  ngOnInit() {}
+}
