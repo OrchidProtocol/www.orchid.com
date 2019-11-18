ac9a25c6011c5ca4fff24ef3975c645c3724da34
calebbabin <object.caleb@gmail.com>
2019-11-18 16:10:56 -0600

    fix typescript indentation

--- src/app/about-company/about-company.component.ts
+++ src/app/about-company/about-company.component.ts
@@ -2,13 +2,13 @@ import { Component, OnInit } from '@angular/core';
 import { MetaService } from '../MetaService';
 
 @Component({
-  selector: 'app-about-company',
-  templateUrl: './about-company.component.html',
-  styleUrls: ['./about-company.component.scss']
+    selector: 'app-about-company',
+    templateUrl: './about-company.component.html',
+    styleUrls: ['./about-company.component.scss']
 })
 export class AboutCompanyComponent implements OnInit {
 
-  constructor( private meta: MetaService ) { }
+    constructor(private meta: MetaService) { }
 
     ngOnInit() {
         this.meta.setGlobalTitle('About the Open Internet | Orchid');
@@ -17,19 +17,19 @@ export class AboutCompanyComponent implements OnInit {
     }
 
     go_sec() {
-	window.location.href = "https://www.sec.gov/Archives/edgar/data/1721086/000172108618000001/xslFormDX01/primary_doc.xml";
+        window.location.href = "https://www.sec.gov/Archives/edgar/data/1721086/000172108618000001/xslFormDX01/primary_doc.xml";
     }
 
     go_press() {
-	let kit = "";
-	
-	if (window && "location" in window && "protocol" in window.location &&
-	    "pathname" in window.location && "host" in window.location) {
-	    kit = window.location.protocol + "//" + window.location.host + "/assets/press/OrchidPressKit.zip";
-	} else {
-	    kit = "https://github.com/orchidprotocol/presskit";
-	}
+        let kit = "";
 
-	window.location.href = kit;
+        if (window && "location" in window && "protocol" in window.location &&
+            "pathname" in window.location && "host" in window.location) {
+            kit = window.location.protocol + "//" + window.location.host + "/assets/press/OrchidPressKit.zip";
+        } else {
+            kit = "https://github.com/orchidprotocol/presskit";
+        }
+
+        window.location.href = kit;
     }
 }
