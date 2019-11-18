f3ed9527b18ce0b0127e10838f4a6dbd50b52204
calebbabin <object.caleb@gmail.com>
2019-11-18 16:55:29 -0600

    add title to privacy policy page

--- src/app/privacy-policy/privacy-policy.component.ts
+++ src/app/privacy-policy/privacy-policy.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MetaService } from '../MetaService';
 
 @Component({
   selector: 'app-privacy-policy',
@@ -7,9 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PrivacyPolicyComponent implements OnInit {
 
-  constructor() { }
+  constructor(private meta: MetaService) { }
 
   ngOnInit() {
+    this.meta.setGlobalTitle('Privacy Policy | Orchid');
   }
 
 }
