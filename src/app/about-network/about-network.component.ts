125ce48f44eac4e210f05e1dca7fea583dff88f4
calebbabin <object.caleb@gmail.com>
2019-11-18 16:56:22 -0600

    add title and description to network page

--- src/app/about-network/about-network.component.ts
+++ src/app/about-network/about-network.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { downloadWhitepaper } from '../whitepaper';
+import { MetaService } from '../MetaService';
 
 @Component({
   selector: 'app-about-network',
@@ -8,9 +9,11 @@ import { downloadWhitepaper } from '../whitepaper';
 })
 export class AboutNetworkComponent implements OnInit {
 
-  constructor() { }
+  constructor(private meta: MetaService) { }
 
   ngOnInit() {
+    this.meta.setGlobalTitle('Distributed VPN | Orchid');
+    this.meta.setGlobalDescription('The Orchid network—an overlay network that runs on the existing internet—is designed for a mutual exchange of benefit in a peer-to-peer marketplace.');
   }
 
   downloadWhitepaper() { downloadWhitepaper(); }
