100060a1c6ad582678335c3fbdd6a16a9c360096
calebbabin <object.caleb@gmail.com>
2019-12-05 15:40:40 -0600

    fix console error relating to loading in i18n json file

--- src/app/i18n/i18n.module.ts
+++ src/app/i18n/i18n.module.ts
@@ -1,18 +1,11 @@
 import { NgModule } from '@angular/core';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { TranslateLoader, TranslateModule, TranslateService } from '@ngx-translate/core';
-import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 
 @NgModule({
   imports: [
     HttpClientModule,
-    TranslateModule.forRoot({
-      loader: {
-        provide: TranslateLoader,
-        useFactory: translateLoaderFactory,
-        deps: [HttpClient]
-      }
-    }),
+    TranslateModule.forRoot({}),
   ],
   exports: [TranslateModule]
 })
@@ -20,10 +13,6 @@ export class I18nModule {
   constructor(translate: TranslateService) {
     translate.addLangs(['en', 'ja', 'ko', 'zh']);
     const browserLang = translate.getBrowserLang();
-    if (browserLang) translate.use(browserLang.match(/en|ja/) ? browserLang : 'en');
+    if (browserLang) translate.use(browserLang.match(/en|ja|ko|zh/) ? browserLang : 'en');
   }
-}
-
-export function translateLoaderFactory(httpClient: HttpClient) {
-  return new TranslateHttpLoader(httpClient);
 }
