c28518de9a242cfe107c72412484feaf7b57f9d5
calebbabin <object.caleb@gmail.com>
2019-12-05 20:32:27 -0600

    temporary fix for oxt.component buttons

--- src/app/oxt/oxt.component.ts
+++ src/app/oxt/oxt.component.ts
@@ -16,4 +16,20 @@ export class OxtComponent implements OnInit {
         this.meta.setGlobalDescription('We believe the internet should be open and accessible to everyone, which is why we\'re building a truly open source network overlay VPN.');
     }
 
+    go_sec() {
+        window.location.href = "https://www.sec.gov/Archives/edgar/data/1721086/000172108618000001/xslFormDX01/primary_doc.xml";
+    }
+
+    go_press() {
+        let kit = "";
+
+        if (window && "location" in window && "protocol" in window.location &&
+            "pathname" in window.location && "host" in window.location) {
+            kit = window.location.protocol + "//" + window.location.host + "/assets/press/OrchidPressKit.zip";
+        } else {
+            kit = "https://github.com/orchidprotocol/presskit";
+        }
+
+        window.location.href = kit;
+    }
 }
