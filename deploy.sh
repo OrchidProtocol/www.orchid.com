1e8c49a6be8c00a46886c874a733e98c985b1092
Nathan Handler <nhandler@orchid.com>
2019-12-11 12:11:31 -0800

    Sync deploy script with master

--- deploy.sh
+++ deploy.sh
@@ -9,18 +9,17 @@ built_files=dist/static
 
 function usage () {
     local exit_status="$1"
-    echo "Usage:  deploy [opus | staging | production]"
+    echo "Usage:  deploy [opuslogica | staging [{ja,ko,zh}] | production [{ja,ko,zh}]"
     echo "Deploys the built website to the specified destination."
     exit $exit_status
 }
 
-if [ "$1" == "" ]; then usage 3; fi
+if [ "$1" == "" ] || [ "$1" == "--help" ] || [ "$1" == "-h" ]; then usage 3; fi
 
 function copy-push () {
     local gitdest="$1"
-    local tempdir="../orchid-www-deploy-$$"
-    rm -rf "$tempdir"
-    (cd ${built_files}; cp -a . ../../"$tempdir")
+    local tempdir=$(mktemp -d)
+    (cd ${built_files}; cp -a . "$tempdir")
     pushd "$tempdir"
     git init
     git add .
@@ -36,7 +35,11 @@ function copy-push () {
 
 # Build the static site...
 rm -rf ${built_files}
-yarn run build:static
+if [ "${#2}" == "2" ]; then
+    yarn run build:static:${2}
+else
+    yarn run build:static
+fi
 (cd ${built_files};
  for file in $(echo *.html); do
      mkdir -p $(basename ${file} .html)
@@ -44,14 +47,14 @@ yarn run build:static
  done;
  ln -s assets/whitepaper/whitepaper.pdf ./whitepaper.pdf)
 
-if [ "$1" == "--help" ]; then
-    usage
-elif [ "$1" == "opuslogica" ]; then
+if [ "$1" == "opuslogica" ]; then
     scp -rp ${built_files}/* .htaccess opuslogica.com:/www/sites/orchid.opuslogica.com/
+elif [ "$1" == "staging" ] && [ "${#2}" == "2" ]; then
+    copy-push "ssh://git-codecommit.us-west-2.amazonaws.com/v1/repos/${2}.orchid.dev"
 elif [ "$1" == "staging" ]; then
     copy-push 'ssh://git-codecommit.us-west-2.amazonaws.com/v1/repos/orchid.dev'
+elif [ "$1" == "production" ] && [ "${#2}" == "2" ]; then
+    copy-push "ssh://git-codecommit.us-west-2.amazonaws.com/v1/repos/${2}.orchid.com"
 elif [ "$1" == "production" ]; then
     copy-push 'ssh://git-codecommit.us-west-2.amazonaws.com/v1/repos/orchid.com'
-elif [ "${#1}" == "2" ]; then
-    copy-push "ssh://git-codecommit.us-west-2.amazonaws.com/v1/repos/${1}.orchid.com"
 fi
