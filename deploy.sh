71406efa24aca1dc022773ca4d84edff743f9424
Nathan Handler <nhandler@orchid.com>
2020-04-07 17:54:15 -0700

    Add support for Atomic Deployments
    This change re-writes the deploy script. We now deploy to a new directory within the AWS S3 bucket with a name based on the git tag/sha. The CloudFront OriginPath is then adjusted to point at the new directory. Finally, we invalidate the cache. This allows for atomic deployments and also allows us to track older versions of the site and rollback to them if necessary.

--- deploy.sh
+++ deploy.sh
@@ -1,61 +1,139 @@
 #!/bin/bash -x
-# deploy.sh: -*- Shell-script -*-  Deploy this website to AWS S3 via CodeCommit.
+# deploy.sh: -*- Shell-script -*-  Deploy this website to AWS S3
 #
 #  Copyright (c) 2019 Brian Fox
-#  Author: Brian Fox (bfox@brianjfox.com)
+#  Copyright (c) 2019-2020 Nathan Handler
+#  Authors: Brian Fox <bfox@brianjfox.com>, Nathan Handler <nhandler@orchid.com>
 #  Birthdate: Mon Mar  4 14:03:59 2019.
 
-built_files=dist/static
 
 function usage () {
     local exit_status="$1"
-    echo "Usage:  deploy [opuslogica | staging [{ja,ko,zh}] | production [{ja,ko,zh}]"
+    echo "Usage:  deploy [staging [{ja,ko,zh,id,ru,en}] | production [{ja,ko,zh,id,ru,en}]"
     echo "Deploys the built website to the specified destination."
     exit $exit_status
 }
 
-if [ "$1" == "" ] || [ "$1" == "--help" ] || [ "$1" == "-h" ]; then usage 3; fi
+function build-site() {
+    local language="$1"
+    echo "Building Site..."
+    yarn
 
-function copy-push () {
-    local gitdest="$1"
-    local tempdir=$(mktemp -d)
-    rsync -av --progress . "$tempdir" --exclude .git --exclude .gitignore
-    pushd "$tempdir"
-    #git init
-    #git add .
-    #git commit -m "deployment"
-    #git remote add aws "$gitdest"
-    #git push --force aws master
-    bucket=$(echo "$gitdest" | sed 's/.*\///')
-    aws s3 sync --acl public-read --delete ./dist/static/ "s3://$bucket"
-    distribution=$(aws cloudfront list-distributions | jq -r --arg bucket "$bucket" '.DistributionList.Items[] | select(.Status=="Deployed") | select(.Aliases.Items[] | contains($bucket)) | .Id')
-    aws cloudfront create-invalidation --distribution-id "$distribution" --paths "/*"
-    popd
-    rm -rf "$tempdir"
+    built_files=dist/static
+    # Build the static site...
+    rm -rf ${built_files}
+    if [ -z "$language" ]; then
+        yarn run build:static
+    else
+        yarn run build:static:${language}
+    fi
+    (cd ${built_files};
+     for file in $(echo *.html); do
+         mkdir -p $(basename ${file} .html)
+         cp ${file} $(basename ${file} .html)/index.html
+     done;
+     ln -s assets/whitepaper/whitepaper.pdf ./whitepaper.pdf)
 }
 
-yarn
+function get-distribution-id() {
+    local bucket="$1"
+    distribution=$(aws cloudfront list-distributions | jq -r --arg bucket "$bucket" '.DistributionList.Items[] | select(.Status=="Deployed") | select(.Aliases.Items[] | contains($bucket)) | .Id')
+    echo "$distribution"
+}
 
-# Build the static site...
-rm -rf ${built_files}
-if [ "${#2}" == "2" ]; then
-    yarn run build:static:${2}
-else
-    yarn run build:static
-fi
-(cd ${built_files};
- for file in $(echo *.html); do
-     mkdir -p $(basename ${file} .html)
-     cp ${file} $(basename ${file} .html)/index.html
- done;
- ln -s assets/whitepaper/whitepaper.pdf ./whitepaper.pdf)
+function upload-site() {
+    local bucket="$1"
+    local version="$2"
+    echo "Uploading Website..."
+    aws s3 sync --acl public-read --delete ./dist/static/ "s3://$bucket/$version"
+}
 
-if [ "$1" == "staging" ] && [ "${#2}" == "2" ]; then
-    copy-push "ssh://git-codecommit.us-west-2.amazonaws.com/v1/repos/www.${2}.orchid.dev"
-elif [ "$1" == "staging" ]; then
-    copy-push 'ssh://git-codecommit.us-west-2.amazonaws.com/v1/repos/www.orchid.dev'
-elif [ "$1" == "production" ] && [ "${#2}" == "2" ]; then
-    copy-push "ssh://git-codecommit.us-west-2.amazonaws.com/v1/repos/www.${2}.orchid.com"
-elif [ "$1" == "production" ]; then
-    copy-push 'ssh://git-codecommit.us-west-2.amazonaws.com/v1/repos/www.orchid.com'
-fi
+function update-distribution() {
+    local distribution="$1"
+    local version="$2"
+    echo "Updating CloudFront Distribution Config..."
+    local config=$(aws cloudfront get-distribution-config --id "$distribution")
+    local etag=$(echo "$config" | jq -r '.ETag')
+    config=$(echo "$config" | jq -r --arg version "$version" '.DistributionConfig.Origins.Items[0].OriginPath = "/"+$version | del(.ETag) | .DistributionConfig')
+    aws cloudfront update-distribution --id "$distribution" --distribution-config "$config" --if-match "$etag"
+}
+
+function wait-for-cloudfront() {
+    local distribution="$1"
+    echo "Waiting for CloudFront Distribution Deployment..."
+    aws cloudfront wait distribution-deployed --id "$distribution"
+}
+
+function invalidate-cache() {
+    local distribution="$1"
+    echo "Invalidating CloudFront Cache..."
+    aws cloudfront create-invalidation --distribution-id "$distribution" --paths "/*"
+}
+
+
+function main() {
+    while getopts "hl:s:v:" OPTION; do
+        case $OPTION in
+            h)
+                usage 3
+                ;;
+            l)
+                local language=$OPTARG
+                [[ ! $language =~ en|ja|ko|zh|id|ru ]] && {
+                    echo "Incorrect language specified!"
+                    usage 3
+                }
+                ;;
+            s)
+                local stage=$OPTARG
+                [[ ! $stage =~ ^staging$|^production$ ]] && {
+                    echo "Incorrect stage specified!"
+                    usage 3
+                }
+                ;;
+            v)
+                local version=$OPTARG
+                ;;
+            *)
+                echo "Incorrect options provided!"
+                usage 3
+                ;;
+        esac
+    done
+
+    if [ -z ${stage+x} ]; then
+        echo "You must specify a stage to deploy to!"
+        usage 3
+    fi
+
+    echo "Language: $language"
+    echo "Stage: $stage"
+    echo "Version: $version"
+
+    if [ -z ${language+x} ]; then  # If a language was NOT specified with -l
+        if [ "$stage" == "staging" ]; then
+            local bucket="www.orchid.dev"
+        elif [ "$stage" == "production" ]; then
+            local bucket="www.orchid.com"
+        fi
+    else  # A language was specified
+        if [ "$stage" == "staging" ]; then
+            local bucket="www.${language}.orchid.dev"
+        elif [ "$stage" == "production" ]; then
+            local bucket="www.${language}.orchid.com"
+        fi
+    fi
+
+    if [ -z ${version+x} ]; then  # If no version was specified with -v
+        version=$(./version.sh | cut -d' ' -f3)
+        echo "Version: $version"
+        build-site "$language"
+        upload-site "$bucket" "$version"
+    fi
+    distribution=$(get-distribution-id "$bucket")
+    update-distribution "$distribution" "$version"
+    wait-for-cloudfront "$distribution"
+    invalidate-cache "$distribution"
+}
+
+main "$@"
