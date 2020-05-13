180669ddb02719704bedffb8221d83b4fece5c6c
calebbabin <object.caleb@gmail.com>
2020-05-13 17:48:51 -0500

    hide the partners page from routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -22,7 +22,7 @@ import { ContactComponent } from './contact/contact.component';
 import { AboutCompanyComponent } from './about-company/about-company.component';
 import { JoinComponent } from './join/join.component';
 import { OxtComponent } from './oxt/oxt.component';
-import { PartnersComponent } from './partners/partners.component';
+import //{ PartnersComponent } from './partners/partners.component';
 import { WhereOXTComponent } from './where-oxt/where-oxt.component';
 import { SetupVideoSectionComponent } from './setup-video-section/setup-video-section.component';
 import { VideoSectionComponent } from './video-section/video-section.component';
