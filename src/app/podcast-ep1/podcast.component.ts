f76245548c7dfbfb090b67bf6bc8550a571867ea
calebbabin <object.caleb@gmail.com>
2020-06-03 15:29:29 -0500

    update episode 1 title and publishing date

--- src/app/podcast-ep1/podcast.component.ts
+++ src/app/podcast-ep1/podcast.component.ts
@@ -11,7 +11,7 @@ export class PodcastEp1Component implements OnInit {
     constructor(private meta: MetaService) { }
 
     ngOnInit() {
-        this.meta.setGlobalTitle('Episode 1: Interview Rich Staropoli | Follow the White Rabbit');
+        this.meta.setGlobalTitle('Episode 1: Interview with Rich Staropoli | Follow the White Rabbit');
         this.meta.setGlobalImage('https://www.orchid.com/assets/img/podcast/EpisodeHeader_RStaropoli.png');
         this.meta.setGlobalDescription('In our premiere episode, we go down the rabbit hole with Rich Staropoli, whose career reads like a Tom Clancy spy novel.');
     }
