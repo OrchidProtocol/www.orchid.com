dc04248e40eea0f59c247c2742c9320e417e8dc7
calebbabin <object.caleb@gmail.com>
2020-06-12 09:23:52 -0500

    make canonical tags more consistent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -25,32 +25,40 @@ export class AppComponent implements OnInit {
   ];
 
   public constructor(
-    private router: Router, 
-    private meta: MetaService, 
-    @Inject(LOCALE_ID) protected localeId: string, 
-    @Inject(DOCUMENT) document, 
+    private router: Router,
+    private meta: MetaService,
+    @Inject(LOCALE_ID) protected localeId: string,
+    @Inject(DOCUMENT) document,
     r: Renderer2
   ) {
-    r.addClass(document.body, `locale-${this.localeId}`);
-    if (this.localeId !== 'en-US') {
-      r.setAttribute(document.querySelector('html'), 'lang', this.localeId);
-      this.createMetaTag(document, r, 'meta', {"rel": "canonical", "href": `https://www.${this.localeId.replace(/en-us/i, 'en')}.orchid.com`+document.location.pathname})
-    } else {
-      this.createMetaTag(document, r, 'meta', {"rel": "canonical", "href": `https://www.orchid.com`+document.location.pathname})
+    document.body.classList.add(`locale-${this.localeId}`);
+
+    const tags_to_remove = document.querySelectorAll('head link[rel="alternate"], head meta[rel="canonical"]');
+    for (let index = 0; index < tags_to_remove.length; index++) {
+      const element = tags_to_remove[index];
+      element.parentNode.removeChild(element);
     }
 
-    
+    const location = document.location._href ? document.location._href : document.location.pathname;
+    if (this.localeId !== 'en-US') {
+      r.setAttribute(document.querySelector('html'), 'lang', this.localeId);
+      this.createMetaTag(document, r, 'meta', { "rel": "canonical", "href": `https://www.${this.localeId.replace(/en-us/i, 'en')}.orchid.com` + location })
+    } else {
+      this.createMetaTag(document, r, 'meta', { "rel": "canonical", "href": `https://www.orchid.com` + location })
+    }
 
-    /*if (this.localeId !== 'en' && this.localeId !== 'en-US')*/ this.createMetaTag(document, r, 'link', {"rel": "alternate", "href": "https://orchid.com"+document.location.pathname, "hreflang": "x-default"})
-    if (this.localeId !== 'ja') this.createMetaTag(document, r, 'link', {"rel": "alternate", "href": "https://ja.orchid.com"+document.location.pathname, "hreflang": "ja"})
-    if (this.localeId !== 'ko') this.createMetaTag(document, r, 'link', {"rel": "alternate", "href": "https://ko.orchid.com"+document.location.pathname, "hreflang": "ko"})
-    if (this.localeId !== 'zh') this.createMetaTag(document, r, 'link', {"rel": "alternate", "href": "https://zh.orchid.com"+document.location.pathname, "hreflang": "zh"})
-    if (this.localeId !== 'ru') this.createMetaTag(document, r, 'link', {"rel": "alternate", "href": "https://ru.orchid.com"+document.location.pathname, "hreflang": "ru"})
-    if (this.localeId !== 'id') this.createMetaTag(document, r, 'link', {"rel": "alternate", "href": "https://id.orchid.com"+document.location.pathname, "hreflang": "id"})
+
+    /*if (this.localeId !== 'en' && this.localeId !== 'en-US')*/ this.createMetaTag(document, r, 'link', { "rel": "alternate", "href": "https://orchid.com" + location, "hreflang": "x-default" })
+    if (this.localeId !== 'ja') this.createMetaTag(document, r, 'link', { "rel": "alternate", "href": "https://ja.orchid.com" + location, "hreflang": "ja" })
+    if (this.localeId !== 'ko') this.createMetaTag(document, r, 'link', { "rel": "alternate", "href": "https://ko.orchid.com" + location, "hreflang": "ko" })
+    if (this.localeId !== 'zh') this.createMetaTag(document, r, 'link', { "rel": "alternate", "href": "https://zh.orchid.com" + location, "hreflang": "zh" })
+    if (this.localeId !== 'ru') this.createMetaTag(document, r, 'link', { "rel": "alternate", "href": "https://ru.orchid.com" + location, "hreflang": "ru" })
+    if (this.localeId !== 'id') this.createMetaTag(document, r, 'link', { "rel": "alternate", "href": "https://id.orchid.com" + location, "hreflang": "id" })
 
   }
 
-  createMetaTag (document, r, type: string, attributes: any) {
+  createMetaTag(document, r, type: string, attributes: any) {
+
     const tag = document.createElement(type);
     for (const key in attributes) {
       if (attributes.hasOwnProperty(key)) {
