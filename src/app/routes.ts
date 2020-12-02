d9c7ce98fdc7bb6bf1cbd9994379c59d32fefab6
Caleb Babin <object.caleb@gmail.com>
2020-12-02 11:14:52 -0600

    publish "Episode 26: Anne Ahola Ward"

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -49,6 +49,7 @@ import { PodcastEp22Component } from './podcast-ep22/podcast.component';
 import { PodcastEp23Component } from './podcast-ep23/podcast.component';
 import { PodcastEp24Component } from './podcast-ep24/podcast.component';
 import { PodcastEp25Component } from './podcast-ep25/podcast.component';
+import { PodcastEp26Component } from './podcast-ep26/podcast.component';
 
 import { NewsletterSignupPage } from './newsletter-signup-page/newsletter-signup-page.component';
 import { WebinarLPComponent } from './webinar-lp/webinar-lp.component';
@@ -143,6 +144,8 @@ export const routes: Routes = [
 			{ path: "podcast/episode-24-brad-kam", component: PodcastEp24Component },
 			{ path: "podcast/episode-25-roger-huang.html", redirectTo: "podcast/episode-25-roger-huang" },
 			{ path: "podcast/episode-25-roger-huang", component: PodcastEp25Component },
+			{ path: "podcast/episode-26-anne-ahola-ward.html", redirectTo: "podcast/episode-26-anne-ahola-ward" },
+			{ path: "podcast/episode-26-anne-ahola-ward", component: PodcastEp26Component },
 			{ path: "newsletter-signup", component: NewsletterSignupPage },
 			{ path: "newsletter-signup.html", redirectTo: 'newsletter-signup' },
 			{ path: "**", component: NotFoundComponent }
