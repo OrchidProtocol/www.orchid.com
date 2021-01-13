f49361826d2ffac4cbc86e46622271e2beafadf4
Caleb Babin <object.caleb@gmail.com>
2021-01-13 13:40:18 -0600

    fix issue with episode title

--- src/app/podcast-ep31/podcast.component.ts
+++ src/app/podcast-ep31/podcast.component.ts
@@ -11,7 +11,7 @@ export class PodcastEp31Component implements OnInit {
     constructor(private meta: MetaService) { }
 
     ngOnInit() {
-        this.meta.setGlobalTitle('Episode 31: ');
+        this.meta.setGlobalTitle('Episode 31: Jamie King');
         this.meta.setGlobalImage('https://www.orchid.com/assets/img/podcast/FTWRSocial_JamieKing.png');
         this.meta.setGlobalDescription('Go down the rabbit hole with Jamie King, Host of Steal this Show and Director of Steal this Film, a documentary on the movement against intellectual property. We take a deep dive into cancel culture, ongoing reality wars, and the consequences of disinformation.');
     }
