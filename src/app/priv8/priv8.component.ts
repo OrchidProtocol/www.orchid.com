91b54bc4a2909bb1aaf85ee1fc5aed363c245574
Caleb Babin <object.caleb@gmail.com>
2021-01-14 16:23:28 -0600

    update priv8 metadata

--- src/app/priv8/priv8.component.ts
+++ src/app/priv8/priv8.component.ts
@@ -14,9 +14,9 @@ export class Priv8 implements OnInit {
     }
 
     ngOnInit() {
-        this.meta.setGlobalTitle('');
-        this.meta.setGlobalImage('https://www.orchid.com/assets/img/podcast/social.png');
-        this.meta.setGlobalDescription('');
+        this.meta.setGlobalTitle('Priv8 | Orchid');
+        this.meta.setGlobalImage('https://www.orchid.com/assets/img/priv8/priv8_social.png');
+        this.meta.setGlobalDescription('Join us for an in-depth discussion on the state of privacy in today’s world with the foremost experts, policymakers, and builders.');
         document.body.classList.add('no-banner');
     }
 }
