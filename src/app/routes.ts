72f9b70977edfd78d4007345754e24eddfeb2110
Caleb Babin <object.caleb@gmail.com>
2021-01-20 12:31:16 -0600

    publish podcast episode 32: Nathan Wilcox

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -55,6 +55,7 @@ import { PodcastEp28Component } from './podcast-ep28/podcast.component';
 import { PodcastEp29Component } from './podcast-ep29/podcast.component';
 import { PodcastEp30Component } from './podcast-ep30/podcast.component';
 import { PodcastEp31Component } from './podcast-ep31/podcast.component';
+import { PodcastEp32Component } from './podcast-ep32/podcast.component';
 
 import { NewsletterSignupPage } from './newsletter-signup-page/newsletter-signup-page.component';
 import { WebinarLPComponent } from './webinar-lp/webinar-lp.component';
@@ -162,6 +163,8 @@ export const routes: Routes = [
 			{ path: "podcast/episode-30-franck-nouyrigat", component: PodcastEp30Component },
 			{ path: "podcast/episode-31-jamie-king.html", redirectTo: "podcast/episode-31-jamie-king" },
 			{ path: "podcast/episode-31-jamie-king", component: PodcastEp31Component },
+			{ path: "podcast/episode-32-nathan-wilcox.html", redirectTo: "podcast/episode-32-nathan-wilcox" },
+			{ path: "podcast/episode-32-nathan-wilcox", component: PodcastEp32Component },
 			{ path: "newsletter-signup", component: NewsletterSignupPage },
 			{ path: "newsletter-signup.html", redirectTo: 'newsletter-signup' },
 			{ path: "quiz", component: QuizComponent },
