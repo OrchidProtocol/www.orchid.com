1713392b644861b39fae48699e4e55d3338519f5
Caleb Babin <object.caleb@gmail.com>
2021-02-03 11:39:11 -0600

    publish "Episode 34: Joe Toscano"

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -57,6 +57,7 @@ import { PodcastEp30Component } from './podcast-ep30/podcast.component';
 import { PodcastEp31Component } from './podcast-ep31/podcast.component';
 import { PodcastEp32Component } from './podcast-ep32/podcast.component';
 import { PodcastEp33Component } from './podcast-ep33/podcast.component';
+import { PodcastEp34Component } from './podcast-ep34/podcast.component';
 
 import { NewsletterSignupPage } from './newsletter-signup-page/newsletter-signup-page.component';
 import { WebinarLPComponent } from './webinar-lp/webinar-lp.component';
@@ -170,6 +171,8 @@ export const routes: Routes = [
 			{ path: "podcast/episode-32-nathan-wilcox", component: PodcastEp32Component },
 			{ path: "podcast/episode-33-john-redgrave.html", redirectTo: "podcast/episode-33-john-redgrave" },
 			{ path: "podcast/episode-33-john-redgrave", component: PodcastEp33Component },
+			{ path: "podcast/episode-34-joe-toscano.html", redirectTo: "podcast/episode-34-joe-toscano" },
+			{ path: "podcast/episode-34-joe-toscano", component: PodcastEp34Component },
 			{ path: "newsletter-signup", component: NewsletterSignupPage },
 			{ path: "newsletter-signup.html", redirectTo: 'newsletter-signup' },
 			{ path: "quiz", component: QuizComponent },
