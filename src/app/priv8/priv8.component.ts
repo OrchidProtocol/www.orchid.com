69a2a1a24f005da059c119832a05b788198cbd9e
Caleb Babin <object.caleb@gmail.com>
2021-03-04 09:49:25 -0600

    add brella widget

--- src/app/priv8/priv8.component.ts
+++ src/app/priv8/priv8.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { MetaService } from '../MetaService';
+
+let brella = false;
 
 @Component({
     selector: 'app-priv8',
@@ -7,6 +9,7 @@ import { MetaService } from '../MetaService';
     styleUrls: ['./priv8.component.scss']
 })
 export class Priv8 implements OnInit {
+    @ViewChild('brellaEmbed', {static: true}) brellaEmbed: ElementRef;
     year: number;
     expandParticipate: boolean = false;
 
@@ -19,6 +22,18 @@ export class Priv8 implements OnInit {
     }
 
     ngOnInit() {
+        this.brellaEmbed.nativeElement.innerHTML = `
+        <div id="brella-widget">
+            <script type="text/props">
+                { "joinCode": "priv8" }
+            </script>
+        </div>`;
+        if (!brella) {
+            brella = true;
+            const script = document.createElement('script');
+            script.setAttribute('src', 'https://widget.brella.io')
+            document.head.appendChild(script);
+        }
         this.meta.setGlobalTitle('Priv8 | Orchid');
         this.meta.setGlobalImage('https://www.orchid.com/assets/img/priv8/priv8_social.png');
         this.meta.setGlobalDescription('Join us for an in-depth discussion on the state of privacy in today’s world with the foremost experts, policymakers, and builders.');
