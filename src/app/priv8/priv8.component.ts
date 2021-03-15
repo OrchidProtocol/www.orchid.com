8c44c6b6fd5ca9e5be4c107ea48674016eead48f
Caleb Babin <object.caleb@gmail.com>
2021-03-15 15:52:39 -0500

    fix navbar styling bug

--- src/app/priv8/priv8.component.ts
+++ src/app/priv8/priv8.component.ts
@@ -117,6 +117,9 @@ export class Priv8 implements OnInit {
         this.meta.setGlobalTitle('Priv8 | Orchid');
         this.meta.setGlobalImage('https://www.orchid.com/assets/img/priv8/priv8_social.png');
         this.meta.setGlobalDescription('Join us for an in-depth discussion on the state of privacy in today’s world with the foremost experts, policymakers, and builders.');
-        document.body.classList.add('no-banner');
+        const doc = typeof document !== "undefined" && document;
+        if (doc) {
+            doc.body.classList.add('no-banner');
+        }
     }
 }
