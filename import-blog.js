c7b30d98952f7dc70cefec4a57e67b5f24605bf8
Caleb Babin <object.caleb@gmail.com>
2021-03-16 16:14:57 -0500

    pull in speakers and images from brella

--- import-blog.js
+++ import-blog.js
@@ -47,6 +47,37 @@ const downloadBlogImage = (url, index = 0) => {
 	return getSmallURL(output);
 }
 
+
+if (!existsSync(join(ASSETS_FOLDER, '/img/brella-integration/')))
+	mkdirSync(join(ASSETS_FOLDER, '/img/brella-integration/'))
+let brellaOffset = 0;
+const downloadBrellaImage = (url) => {
+	const output = join(ASSETS_FOLDER, '/img/brella-integration/', url.split('/')[url.split('/').length - 1]);
+
+	// introduce a timeout to prevent script from freezing during disk access issues
+	setTimeout(() => {
+		const stream = createWriteStream(output);
+		fetch(url)
+			.then(res => {
+				res.body.pipe(stream);
+				stream.on('close', () => {
+					console.log('downloaded', url);
+					sharp(output)
+						.resize(120, 120)
+						.jpeg({ quality: 75 })
+						.toFile(getSmallURL(output), (err, info) => {
+							if (err) console.log(err, info);
+						});
+				})
+			})
+			.catch(e => {
+				throw e;
+			})
+	}, brellaOffset++ * 100);
+
+	return getSmallURL(output);
+}
+
 let domain = 'blog.orchid.com';
 if (process.env.BUILD_LOCALE) {
 	domain = domain.replace('blog.', `blog.${process.env.BUILD_LOCALE}.`);
@@ -75,7 +106,49 @@ writeFileSync(join(ASSETS_FOLDER, 'brella-priv8.json'), JSON.stringify({}));
 fetch(`https://api.brella.io/api/aalto/events/priv8/time_slots`)
 .then(response => response.json())
 .then(data => {
+
+	const output = {};
+	for (let index = 0; index < data.included.length; index++) {
+		const element = data.included[index];
+		if (element && element.attributes && element.attributes['photo-url']) {
+			const imagePath = downloadBrellaImage(element.attributes['photo-url']);
+			let name = '';
+			if (element.attributes['honorific'] !== null) {
+				name += element.attributes['honorific'] + ' ';
+			}
+			if (element.attributes['first-name'] !== null) {
+				name += element.attributes['first-name']
+			}
+			if (element.attributes['middle-name'] !== null) {
+				name += ' ' + element.attributes['middle-name']
+			}
+			if (element.attributes['last-name'] !== null) {
+				name += ' ' + element.attributes['last-name']
+			}
+			name = name.replace(/  /g, ' ');
+			output[element.id] = {
+				name,
+				image: imagePath.split('/')[imagePath.split('/').length - 1],
+			}
+		} else {
+			output[element.id] = {
+				type: element.type,
+				role: element.attributes.role,
+				speaker: element.relationships.speaker.data.id
+			};
+		}
+	}
+	for (const key in output) {
+		if (Object.hasOwnProperty.call(output, key)) {
+			if (output[key].speaker) {
+				output[key].name = output[output[key].speaker].name;
+				output[key].image = output[output[key].speaker].image;
+			}
+		}
+	}
+
 	try {
+		data.speakers = output;
 		writeFileSync(join(ASSETS_FOLDER, 'brella-priv8.json'), JSON.stringify(data));
 	} catch (e) { }
 })
