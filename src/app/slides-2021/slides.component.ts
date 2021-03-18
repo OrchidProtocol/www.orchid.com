eac0566223859a08246b6c132c7b61f663f79657
Caleb Babin <object.caleb@gmail.com>
2021-03-18 10:37:11 -0500

    add a brief title and description

--- src/app/slides-2021/slides.component.ts
+++ src/app/slides-2021/slides.component.ts
@@ -15,6 +15,7 @@ export class Slides2021Component implements OnInit {
     }
 
     ngOnInit() {
-        this.meta.setGlobalTitle('2021 Slides | Orchid');
+        this.meta.setGlobalTitle('Decentralization is the new encryption | Orchid');
+        this.meta.setGlobalDescription('Decentralization is the new encryption presentation - how decentralized and encrypted technologies complement each other for better privacy solutions.');
     }
 }
