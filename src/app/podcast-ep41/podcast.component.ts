d45330275e01880f12023b728bfc4f64c7ef6254
Caleb Babin <object.caleb@gmail.com>
2021-03-23 19:11:38 -0500

    add images to latest podcast episode

--- src/app/podcast-ep41/podcast.component.ts
+++ src/app/podcast-ep41/podcast.component.ts
@@ -12,7 +12,7 @@ export class PodcastEp41Component implements OnInit {
 
     ngOnInit() {
         this.meta.setGlobalTitle('Episode 41: Aibek Esengulov');
-        this.meta.setGlobalImage('https://www.orchid.com/assets/img/podcast/');
+        this.meta.setGlobalImage('https://www.orchid.com/assets/img/podcast/AibekEsengulov_Social.png');
         this.meta.setGlobalDescription('Go down the rabbit hole with Aibek Esengulov, Founder of Horizontal Systems, the developers of the privacy-focused wallet, Unstoppable. We take a deep dive into the DEFI revolution, investing without borders, and the rise of digital collectibles.');
     }
 }
