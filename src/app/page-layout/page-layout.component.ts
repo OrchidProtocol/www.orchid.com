03cb32247f96487eea1c2ada5ea21bb4424a200a
Caleb Babin <object.caleb@gmail.com>
2021-04-07 15:42:52 -0500

    properly pass UTM parameters to google play links

--- src/app/page-layout/page-layout.component.ts
+++ src/app/page-layout/page-layout.component.ts
@@ -165,12 +165,19 @@ export class PageLayoutComponent implements OnInit {
       window.addEventListener("resize", checkShadow);
 
       if (typeof window !== "undefined" && window['landingQueryParams']) {
-      if (window['landingQueryParams']) {
-        const appstore_links = doc.querySelectorAll('a[href^="https://apps.apple.com"], a[href^="https://play.google.com"]');
+        const appstore_links = doc.querySelectorAll('a[href^="https://apps.apple.com"]');
         for (let index = 0; index < appstore_links.length; index++) {
           const element = appstore_links[index];
           let url = element.getAttribute('href');
           url = url.split('?')[0] + window['landingQueryParams'];
+          element.setAttribute('href', url);
+        }
+
+        const googleplay_links = doc.querySelectorAll('a[href^="https://play.google.com"]');
+        for (let index = 0; index < googleplay_links.length; index++) {
+          const element = googleplay_links[index];
+          let url = element.getAttribute('href');
+          url = url.split('&')[0] + window['landingQueryParams'].replace('?', '&');
           element.setAttribute('href', url);
         }
       }
