a168b13fb626ccaaffacccfc6bbe729302b9a3b9
Caleb Babin <object.caleb@gmail.com>
2021-09-01 12:37:56 -0500

    publish podcast "Episode 63: Julie Owono"

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -93,6 +93,7 @@ import { PodcastEp59Component } from './podcast-ep59/podcast.component';
 import { PodcastEp60Component } from './podcast-ep60/podcast.component';
 import { PodcastEp61Component } from './podcast-ep61/podcast.component';
 import { PodcastEp62Component } from './podcast-ep62/podcast.component';
+import { PodcastEp63Component } from './podcast-ep63/podcast.component';
 
 import { WhereOXTComponent } from './where-oxt/where-oxt.component';
 import { WhereOXTExtendedComponent } from './where-oxt-extended/where-oxt.component';
@@ -202,6 +203,7 @@ import { MetaService } from './MetaService';
     PodcastEp60Component,
     PodcastEp61Component,
     PodcastEp62Component,
+    PodcastEp63Component,
     NewsletterSignupPage,
     WhereOXTComponent,
     WhereOXTExtendedComponent,
