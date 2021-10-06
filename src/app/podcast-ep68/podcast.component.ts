3079786bb9a7fdd7b997191768407a7cb15b74ce
Caleb Babin <object.caleb@gmail.com>
2021-10-06 11:55:11 -0500

    publish podcast "Episode 68: Julian Zatowski"

---
+++ src/app/podcast-ep68/podcast.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { MetaService } from '../MetaService';
+
+@Component({
+	selector: 'app-podcast',
+	templateUrl: './podcast.component.html',
+	styleUrls: ['../podcast/podcast.component.scss']
+})
+export class PodcastEp68Component implements OnInit {
+
+	constructor(private meta: MetaService) { }
+
+	ngOnInit() {
+		this.meta.setGlobalTitle('Episode 68: Julian Zatowski');
+		this.meta.setGlobalImage('https://www.orchid.com/assets/img/podcast/P8PEpisode_JulianZatowski.png');
+		this.meta.setGlobalDescription('Host Derek E. Silva joins Julian Zawistowski, Director of Golem Foundation, a decentralized marketplace for computing power. A great conversation on the power of decentralized governance, breaking corporate data silos, and empowering individuals to put privacy first by controlling their own data.');
+	}
+}
