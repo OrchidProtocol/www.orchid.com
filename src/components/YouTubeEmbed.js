e5697780bddff8b5df9e5708fff4caab0530da35
calebbabin <object.caleb@gmail.com>
2022-01-20 17:29:34 -0600

    use a common video component

---
+++ src/components/YouTubeEmbed.js
@@ -0,0 +1,110 @@
+import React from 'react'
+import styled from '@emotion/styled';
+
+const VideoContainer = styled.div`
+	& > section {
+		box-sizing: border-box;
+		max-width: 720px;
+	}
+
+	//&[data-defaultthumb] {
+		a {
+			background-color: #000;
+			background-image: url(/img/index/video-thumbnail.jpg);
+			background-size: cover;
+			background-position: center;
+			background-repeat: no-repeat;
+		}
+	//}
+	.video {
+		position: relative;
+		padding-bottom: 56.25%;
+		height: 0;
+		width: 100%;
+		border-radius: 24px;
+		overflow: hidden;
+
+		& > iframe {
+			position: absolute;
+			top: 0;
+			left: 0;
+			width: 100%;
+			height: 100%;
+			border: 0;
+			z-index: 999;
+		}
+
+		& > a {
+			display: block;
+			position: absolute;
+			top: 0;
+			left: 0;
+			width: 100%;
+			height: 100%;
+			font-size: 0;
+
+			&::before,
+			&::after {
+				content: "";
+				display: block;
+				position: absolute;
+				top: 0;
+				left: 0;
+				width: 100%;
+				height: 100%;
+			}
+			&::before {
+				background-color: rgba(0, 0, 0, 0.1);
+				box-shadow: 0px 0px 10% rgba(0, 0, 0, 0.2) inset;
+			}
+			&::after {
+				background-size: auto 50px;
+				@media (max-width: 960px) {
+					background-size: auto 35px;
+				}
+				background-position: center;
+				background-repeat: no-repeat;
+				background-image: url(/img/index/yt_icon_mono_light.png);
+			}
+			&:hover::after {
+				background-image: url(/img/index/yt_icon_rgb.png);
+			}
+		}
+	}
+`;
+
+class Component extends React.Component {
+	constructor() {
+		super();
+		this.state = {
+			js: false,
+			showVideo: false,
+		};
+	}
+	componentDidMount() {
+		this.setState({
+			js: true,
+		});
+	}
+	revealVideo(e) {
+		e.preventDefault();
+		this.setState({
+			showVideo: true,
+		});
+	}
+	render() {
+		return (
+			<VideoContainer className={(this.state.js ? 'js' : '')} data-defaultthumb={this.props.defaultThumbnail}>
+				<section className="section-narrow hgap-thin vgap-wide z-index-1 center-text">
+					<div className="video">
+						{this.state.showVideo ?
+							<iframe allowFullScreen title={"YouTube video embed " + this.props.id} src={"https://www.youtube-nocookie.com/embed/" + this.props.id + "?autoplay=true"} /> :
+							<a onClick={this.revealVideo.bind(this)} href={"https://www.youtube.com/watch?v=" + this.props.id}>Watch a video about Orchid on YouTube</a>}
+					</div>
+				</section>
+			</VideoContainer>
+		)
+	}
+}
+
+export default Component
