a1a87ab85b1462d3262c39ceb19e7630a05d1df1
calebbabin <object.caleb@gmail.com>
2022-01-24 11:37:12 -0600

    install purge CSS plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -93,5 +93,20 @@ module.exports = {
 				addUncaughtPages: true,
 			},
 		},
+		{
+			resolve: `gatsby-plugin-purgecss`,
+			options: {
+				// printRejected: true, // Print removed selectors and processed file names
+				// develop: true, // Enable while using `gatsby develop`
+				// tailwind: true, // Enable tailwindcss support
+				// ignore: ['/ignored.css', 'prismjs/', 'docsearch.js/'], // Ignore files/folders
+				// purgeOnly : ['components/', '/main.css', 'bootstrap/'], // Purge only these files/folders
+				// purgeCSSOptions: {
+				// https://purgecss.com/configuration.html#options
+				// safelist: ['safelist'], // Don't remove this selector
+				// },
+				// More options defined here https://purgecss.com/configuration.html#options
+			},
+		},
 	],
 }
