c12dc486e3c260a6d9a8148a8d5b5f27e1669975
calebbabin <object.caleb@gmail.com>
2022-02-21 10:21:28 -0600

    add tailwind

--- gatsby-config.js
+++ gatsby-config.js
@@ -41,7 +41,11 @@ module.exports = {
 			options: {
 				sassOptions: {
 					includePaths: ["src/scss"],
-				}
+				},
+				postCssPlugins: [
+					require("tailwindcss"),
+					require("./tailwind.config.js"), // Optional: Load custom Tailwind CSS configuration
+				],
 			},
 		},
 		{
@@ -123,8 +127,8 @@ module.exports = {
 			resolve: `gatsby-plugin-purgecss`,
 			options: {
 				// printRejected: true, // Print removed selectors and processed file names
-				// develop: true, // Enable while using `gatsby develop`
-				// tailwind: true, // Enable tailwindcss support
+				develop: true, // Enable while using `gatsby develop`
+				tailwind: true, // Enable tailwindcss support
 				// ignore: ['/ignored.css', 'prismjs/', 'docsearch.js/'], // Ignore files/folders
 				// purgeOnly : ['components/', '/main.css', 'bootstrap/'], // Purge only these files/folders
 				// purgeCSSOptions: {
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+	content: [
+		'./src/**/*.{md,mdx,js,jsx,ts,tsx}',
+	],
+	theme: {
+		extend: {
+			colors: {
+				'gray': 'var(--orc-rock-gray)',
+			}
+		},
+	},
+	plugins: [],
+}
