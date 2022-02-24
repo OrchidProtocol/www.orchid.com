83290a7df9e3114c6d58721c6b4883ef57f34177
calebbabin <object.caleb@gmail.com>
2022-02-24 11:26:44 -0600

    disable purgeCSS develop mode

--- gatsby-config.js
+++ gatsby-config.js
@@ -127,7 +127,7 @@ module.exports = {
 			resolve: `gatsby-plugin-purgecss`,
 			options: {
 				// printRejected: true, // Print removed selectors and processed file names
-				develop: true, // Enable while using `gatsby develop`
+				//develop: true, // Enable while using `gatsby develop`
 				tailwind: true, // Enable tailwindcss support
 				// ignore: ['/ignored.css', 'prismjs/', 'docsearch.js/'], // Ignore files/folders
 				// purgeOnly : ['components/', '/main.css', 'bootstrap/'], // Purge only these files/folders
