98efc5656145b3a62414f5017596ffa9d7050c48
calebbabin <object.caleb@gmail.com>
2022-03-09 17:13:43 -0600

    update VPN copy and sections to match figma

--- src/pages/vpn/index.js
+++ src/pages/vpn/index.js
@@ -105,19 +105,14 @@ function Page(props) {
 								width: 3rem;
 							}
 						}
-					`}>Don't be<span className="spacer" /><br /><span className="spacer" />a product</h1>
+					`}>UNBLOCK<br />YOUR INTERNET.</h1>
 
 					<h3>
-						<span style={{ display: "inline-block" }}><Trans>No data collection.</Trans></span>
-						<span style={{ display: "inline-block" }}><Trans>No 3rd-party sharing.</Trans></span>
-						<span style={{ display: "inline-block" }}><Trans>No profit markup.</Trans></span>
+						<Trans>The web has evolved. <span className='text-purple'>So should your VPN.</span></Trans>
 					</h3>
-					<h3 className="color-primary"><span><Trans>A new model of VPN.</Trans></span></h3>
 					<Link to="/get-orchid" className="btn">
 						<button className="btn-primary"><Trans>Get Orchid</Trans></button>
 					</Link>
-					<br />
-					<small><Trans>(starting at just $1)</Trans></small>
 				</div>
 				<div css={css`
 						width: 400px;
@@ -154,7 +149,7 @@ function Page(props) {
 
 			<section className="marketplace__wrapper">
 				<div className="marketplace__container container-common">
-					<h2 className="section-heading"><Trans>Orchid is a decentralized bandwidth marketplace</Trans></h2>
+					<h2 className="section-heading"><Trans>Orchid VPN enables a bandwidth marketplace</Trans></h2>
 					<p>
 						<Trans>
 							Buyers use the <Link to="/download">Orchid app</Link> to get protection from snooping ISP's, unblock websites cutoff by firewalls, and for many other privacy benefits. Sellers receive payments in real-time as they provide service and stake <Link to="/oxt">OXT</Link> to compete for service requests. Orchid Technologies charges no fees, and the community maintains the <a href="https://github.com/OrchidTechnologies">Open Source software</a> that powers the entire ecosystem. Try it out!
@@ -276,114 +271,9 @@ function Page(props) {
 				</div>
 			</section>
 
-			<div className="wavy-element-05"></div>
-
-			<section className="press-wrapper">
-				<div className="press-container container-common">
-					<h2 className="section-heading"><Trans>Orchid News</Trans></h2>
-					<div className="press-list">
-						<div className="press-card">
-							<p className="press-card__quote">
-								In a world where one assumes the Cambridge Analytica scandal is merely the tip of the iceberg
-								when it comes to data abuse, it’s easy to see the project’s appeal.
-							</p>
-							<a href="https://techcrunch.com/2018/04/20/orchid-labs-is-in-the-process-of-raising-125-million-for-its-surveillance-free-layer-atop-the-internet/" className="footer">
-								<div className="logo">
-									<img width="360" height="51" src="/img/vpn/seen/techcrunch.png" alt="techcrunch" loading="lazy" />
-								</div>
-								<div className="uppercase">TECHCRUNCH</div>
-								<img src="/img/vpn/read-more-arrow.svg" width="42" height="16" alt="" loading="lazy" />
-							</a>
-						</div>
-						<div className="press-card">
-							<p className="press-card__quote">
-								This is what the future of VPN tech looks like. And you gotta see it.
-							</p>
-							<a href="https://www.cnet.com/news/orchid-vpn-review-it-uses-the-tech-behind-bitcoin-to-improve-privacy/" className="footer">
-								<div className="logo">
-									<img width="180" height="132" src="/img/vpn/seen/cnet.svg" alt="CNET" loading="lazy" />
-								</div>
-								<div className="uppercase">CNET</div>
-								<img src="/img/vpn/read-more-arrow.svg" width="42" height="16" alt="" loading="lazy" />
-							</a>
-						</div>
-						<div className="press-card">
-							<p className="press-card__quote">
-								Orchid’s mission is to build open-source software that keeps the Internet open and accessible  —
-								a natural resource for everyone, everywhere.
-							</p>
-							<a href="https://www.coindesk.com/orchid-tops-43-million-raised-in-token-sale-for-crypto-tor-alternative" className="footer">
-								<div className="logo">
-									<img width="360" height="70" src="/img/vpn/seen/coindesk.png" alt="coindesk" loading="lazy" />
-								</div>
-								<div className="uppercase">COINDESK</div>
-								<img src="/img/vpn/read-more-arrow.svg" width="42" height="16" alt="" loading="lazy" />
-							</a>
-						</div>
-					</div>
-					<h3 className="center-text vpad-thin color-gray" style={{ marginBottom: "-0.5rem" }}>The Orchid community has a lot to say as well!</h3>
-					<div className="press-list">
-						<div className="press-card">
-							<p className="press-card__quote">
-								"Orchid’s VPN could be very useful from a point of view of freedom."
-								<br /><br />
-								<a href="https://twitter.com/search?q=%24OXT&amp;src=cashtag_click">$OXT</a>&nbsp;<a href="https://twitter.com/hashtag/OrchidProtocol?src=hashtag_click">#OrchidProtocol</a>
-								<br /><br />
-								Excellent read: <a href="https://beincrypto.com/everything-is-an-economy-orchid-ceo-steven-waterhouse/">Orchid CEO Steven Waterhouse on VPNs...</a>
-							</p>
-							<div className="footer">
-								<div className="logo offset">
-									<img src="/img/vpn/TweetBunny.png" loading="lazy" width="273" height="201" alt="" />
-								</div>
-								<a className="press-card__more uppercase color-gray" href="https://twitter.com/justinsanger/status/1380986189503664131">@justinsanger</a>
-							</div>
-						</div>
-						<div className="press-card">
-							<p className="press-card__quote">
-								So proud of <a href="https://twitter.com/OrchidProtocol">@OrchidProtocol</a> getting listed in the Apple app store! Privacy and internet access should be synonymous, and Orchid's decentralized VPN is a key step forward. I'm going to try a week of <a href="https://twitter.com/hashtag/OrchidAlwaysOn?src=hashtag_click">#OrchidAlwaysOn</a> and keep my VPN running in the background.
-							</p>
-							<div className="footer">
-								<div className="logo offset">
-									<img src="/img/vpn/TweetBunny.png" loading="lazy" width="273" height="201" alt="" />
-								</div>
-								<a className="press-card__more uppercase color-gray" href="https://twitter.com/collectionist/status/1281442941186129920">@collectionist</a>
-							</div>
-						</div>
-					</div>
-				</div>
-			</section>
-
-			<div className="wavy-element-06"></div>
-
-			{/*<div className="wavy-element-07"></div>*/}
-
 			<section className="newsletter-wrapper">
 				<div className="newsletter-container container-common">
-					<h2 className="section-heading"><Trans>Connect</Trans></h2>
-					<p><Trans>The Orchid community is one of the things that makes this project truly special. Follow us through one of our many social platforms, meet us in person at one of our upcoming events, and subscribe to our newsletter to stay informed about all things Orchid!</Trans></p>
-
-					<div className="social-icons">
-						<a className="inline-block" href="https://twitter.com/OrchidProtocol" target="_blank" rel="noopener noreferrer" aria-label="Follow on Twitter" title="Twitter">
-							<img src="/img/shared/social-twitter-purple.svg" alt="Twitter" width="32" height="32" />
-						</a>
-						<a className="inline-block" href="https://discord.gg/GDbxmjxX9F" target="_blank" rel="noopener noreferrer" aria-label="Join us on Discord" title="Discord">
-							<img src="/img/shared/social-discord-purple.svg" alt="Discord" width="32" height="32" />
-						</a>
-						<a className="inline-block" href="https://www.t.me/OrchidOfficial" target="_blank" rel="noopener noreferrer" aria-label="Join us on Telegram" title="Telegram">
-							<img src="/img/shared/social-telegram-purple.svg" alt="Telegram" width="32" height="32" />
-						</a>
-						<a className="inline-block" href="https://github.com/OrchidTechnologies" target="_blank" rel="noopener noreferrer" aria-label="See the Source" title="Github">
-							<img src="/img/shared/social-github-purple.svg" alt="Github" width="32" height="32" />
-						</a>
-						<a className="inline-block" href="https://www.facebook.com/OrchidProtocol" target="_blank" rel="noopener noreferrer" aria-label="Follow on Facebook" title="Facebook">
-							<img src="/img/shared/social-facebook-purple.svg" alt="Facebook" width="32" height="32" />
-						</a>
-						<a className="inline-block" href="https://www.reddit.com/r/orchid/" target="_blank" rel="noopener noreferrer" aria-label="Check out our Subreddit" title="Reddit">
-							<img src="/img/shared/social-reddit-purple.svg" alt="Reddit" width="32" height="32" />
-						</a>
-					</div>
-
-					<h3><Trans>Let’s stay connected</Trans></h3>
+					<h2 className="section-heading"><Trans>Let’s stay connected</Trans></h2>
 					<p><Trans>Get the Orchid Onlooker newsletter for updates and privacy news</Trans></p>
 					<NewsletterSignupCore t={t} largePadding="true" />
 				</div>
