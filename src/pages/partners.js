9028d6d8f4ce6a752692691f87ce019d32e9fb7e
calebbabin <object.caleb@gmail.com>
2022-03-15 14:54:30 -0500

    fix "learn more" not translating

--- src/pages/partners.js
+++ src/pages/partners.js
@@ -60,6 +60,7 @@ function Page(props) {
 												We value PIA's commitment to maintaining the highest standards of privacy, while simultaneously providing one of the most reliable VPN services in the world.
 											</Trans>
 										</p>
+										<span class="learnMore"><Trans>Learn More</Trans></span>
 									</div>
 								</a>
 							</div>
@@ -75,6 +76,7 @@ function Page(props) {
 												The alliance with BolehVPN, one of the most trusted names in trusted browsing, brings the dream of a genuinely secure and private internet closer to fruition.
 											</Trans>
 										</p>
+										<span class="learnMore"><Trans>Learn More</Trans></span>
 									</div>
 								</a>
 							</div>
@@ -90,6 +92,7 @@ function Page(props) {
 												At Orchid, we’re always happy to partner with like-minded firms and individuals, and for years we’ve admired the work that VPNSecure has done in the privacy and security space.
 											</Trans>
 										</p>
+										<span class="learnMore"><Trans>Learn More</Trans></span>
 									</div>
 								</a>
 							</div>
@@ -105,6 +108,7 @@ function Page(props) {
 												Orchid’s privacy network is growing every day, with more and more reliable bandwidth providers joining the platform. Our partnership with LiquidVPN is another step toward making privacy a community experience.
 											</Trans>
 										</p>
+										<span class="learnMore"><Trans>Learn More</Trans></span>
 									</div>
 								</a>
 							</div>
@@ -135,6 +139,7 @@ function Page(props) {
 												Available as a browser extension and as a mobile app, MetaMask equips you with a key vault, secure login, and token wallet—everything you need to manage your digital assets.
 											</Trans>
 										</p>
+										<span class="learnMore"><Trans>Learn More</Trans></span>
 									</div>
 								</a>
 							</div>
@@ -150,6 +155,7 @@ function Page(props) {
 												imToken has done great work in building a secure and user-friendly product… users will now have an easy way to hold their OXT and use it to buy private bandwidth.
 											</Trans>
 										</p>
+										<span class="learnMore"><Trans>Learn More</Trans></span>
 									</div>
 								</a>
 							</div>
@@ -165,6 +171,7 @@ function Page(props) {
 												TrustWallet is a leader in cryptocurrency wallets; integration with the app means that many more users will have an easy way to hold, trade, and transfer OXT.
 											</Trans>
 										</p>
+										<span class="learnMore"><Trans>Learn More</Trans></span>
 									</div>
 								</a>
 							</div>
@@ -180,6 +187,7 @@ function Page(props) {
 												Our goal is to make the internet a safer, more free place. BitGo provides that same sense of security to our institutional partners, in a streamlined experience.
 											</Trans>
 										</p>
+										<span class="learnMore"><Trans>Learn More</Trans></span>
 									</div>
 								</a>
 							</div>
@@ -195,6 +203,7 @@ function Page(props) {
 												BRD has built a huge community of crypto enthusiasts, many of whom are committed to taking control of their privacy. Now, they can access OXT and do exactly that.
 											</Trans>
 										</p>
+										<span class="learnMore"><Trans>Learn More</Trans></span>
 									</div>
 								</a>
 							</div>
@@ -210,6 +219,7 @@ function Page(props) {
 												Ledger wallets have earned their incredible popularity, and we’re very excited that our community will be able to store their OXT on a trusted Ledger device.
 											</Trans>
 										</p>
+										<span class="learnMore"><Trans>Learn More</Trans></span>
 									</div>
 								</a>
 							</div>
@@ -239,6 +249,7 @@ function Page(props) {
 												Opening our privacy network to the people of New York is an important step in solidifying our global community. We look forward to making the internet open for everyone.
 											</Trans>
 										</p>
+										<span class="learnMore"><Trans>Learn More</Trans></span>
 									</div>
 								</a>
 							</div>
@@ -254,6 +265,7 @@ function Page(props) {
 												Making OXT widely available for our users in Europe and around the world is an important step in making it easy for anyone to start using it to pay for private bandwidth.
 											</Trans>
 										</p>
+										<span class="learnMore"><Trans>Learn More</Trans></span>
 									</div>
 								</a>
 							</div>
@@ -270,6 +282,7 @@ function Page(props) {
 												Gemini's established history as a globally licensed, trusted name in crypto markets make them a perfect partner for Orchid as we continue making OXT widely available.
 											</Trans>
 										</p>
+										<span class="learnMore"><Trans>Learn More</Trans></span>
 									</div>
 								</a>
 							</div>
@@ -285,6 +298,7 @@ function Page(props) {
 												OKEx listing Orchid makes it easier for international users to acquire OXT and start using it to access private bandwidth; this is an important day for the growth of internet freedom.
 											</Trans>
 										</p>
+										<span class="learnMore"><Trans>Learn More</Trans></span>
 									</div>
 								</a>
 							</div>
@@ -300,6 +314,7 @@ function Page(props) {
 												Our community can feel confident when purchasing OXT on a platform with a proven track record of regulatory compliance and security best practices.
 											</Trans>
 										</p>
+										<span class="learnMore"><Trans>Learn More</Trans></span>
 									</div>
 								</a>
 							</div>
@@ -330,6 +345,7 @@ function Page(props) {
 												Bloq is a key partner in our goal of creating a community that makes the Internet a more free and private place.
 											</Trans>
 										</p>
+										<span class="learnMore"><Trans>Learn More</Trans></span>
 									</div>
 								</a>
 							</div>
@@ -344,6 +360,7 @@ function Page(props) {
 												The Blockchain Association comprises many of the most visionary projects, firms, and institutions in blockchain.
 											</Trans>
 										</p>
+										<span class="learnMore"><Trans>Learn More</Trans></span>
 									</div>
 								</a>
 							</div>
@@ -359,6 +376,7 @@ function Page(props) {
 												We’re thrilled to work with some of the most forward-thinking minds in tech policy to make the decentralized web a reality.
 											</Trans>
 										</p>
+										<span class="learnMore"><Trans>Learn More</Trans></span>
 									</div>
 								</a>
 							</div>
